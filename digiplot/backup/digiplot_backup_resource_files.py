4624553c2cbcceb0975789627e7a9b3d6d4876c9
sonofeft <ctatsourceforge@gmail.com>
2017-07-03 17:36:08 -0700

    Removed backup directory from project files

--- digiplot/backup/digiplot_backup_resource_files.py
+++
@@ -1,7 +0,0 @@
-
-
-# parent_dir = D:\py_proj_2017\DigiPlot\digiplot
-target_dir = r"backup"
-
-file_typeD = {} # file type description. index=resource path, value=mime type
-file_typeD[r"sample_poly.png"]="('image/png', None)"
